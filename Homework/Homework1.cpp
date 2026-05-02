5bf9d056050770b40ba4cd4f8dac39735719e4ea
Ra-cloud08 <thounsoktheara60@gmail.com>
2026-05-02 08:37:33 +0700

    CPP_CNXA_COST

--- Homework/Homework1.cpp
+++ Homework/Homework1.cpp
@@ -1,6 +1,7 @@
 /*write a small program to manage the flow of trade in beverage shop
     cofee = 2.5$
     juice = 3.35$
+    matcha = 3$
     coffe and juice has in stock 10 glasses
 */
 
@@ -10,15 +11,17 @@ int main(){
 
     std::cout<<std::endl;
     std::cout<<"======= Welcome ======"<<std::endl;
-    float Coffe , Juice;
+    float Coffe , Juice, Matcha;
     //costumer oder
     std::cout<<"The costumer buy coffe: ";
     std::cin>>Coffe;
-    std::cout<<"The costumer buy juice: ";
+    std::cout<<"The costumehr buy juice: ";
     std::cin>>Juice;
+    std::cout<<"The costumehr buy matcha: ";
+    std::cin>>Matcha;
 
     //total drink sold
-    float totalitem = Coffe + Juice;
+    float totalitem = Coffe + Juice + Matcha;
     std::cout<<"Total drinks order: "<<totalitem<<" glasses"<<std::endl;
     std::cout<<std::endl;
     
@@ -26,11 +29,14 @@ int main(){
     std::cout<<"======= Charge ======"<<std::endl;
     float coffeprice = Coffe * 2.5;
     float juiceprice = Juice * 3.35;
+    float matchaprice = Matcha * 3;
     std::cout<<"coffe total price: "<<coffeprice<<"$"<<std::endl;
     std::cout<<"juice total price: "<<juiceprice<<"$"<<std::endl;
+    std::cout<<"matcha total price: "<<matchaprice<<"$"<<std::endl;
+    
 
     //total price
-    float totalprice = coffeprice + juiceprice;
+    float totalprice = coffeprice + juiceprice + matchaprice;
     std::cout<<"Total price: "<<totalprice<<"$"<<std::endl;
     std::cout<<std::endl;
     
@@ -51,22 +57,27 @@ int main(){
 
     //stock report
     std::cout<<"======= Stock report ======="<<std::endl;
-    int coffestock = 10, juicestock = 10;
+    int coffestock = 10, juicestock = 14, matchastock = 12;
         if(Coffe <= 10){
         coffestock -= Coffe;
         }
-        if(Juice <= 10){
+        if(Juice <= 20){
         juicestock -= Juice;
+        }
+        if(Matcha <= 20){
+            matchastock -= Matcha;
         }
     std::cout<<"Remaining coffe stock: "<<coffestock<<" glasses"<<std::endl;
     std::cout<<"Remaining juice stock: "<<juicestock<<" glasses"<<std::endl;
+    std::cout<<"Remaining matcha stock: "<<matchastock<<" glasses"<<std::endl;
     std::cout<<std::endl;
 
     //stock update
     std::cout<<"======= stock update ======="<<std::endl;
-    coffestock += Coffe, juicestock += Juice;
+    coffestock += Coffe, juicestock += Juice, matchastock += Matcha;
     std::cout<<"Update coffe stock: "<<coffestock<<" glasses"<<std::endl;
     std::cout<<"Update juice stock: "<<juicestock<<" glasses"<<std::endl;
+    std::cout<<"Update matcha stock: "<<matchastock<<" glasses"<<std::endl;
     std::cout<<std::endl;
     
 
